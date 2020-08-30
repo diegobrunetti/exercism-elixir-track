aabf03592c5eacaca6c18b0756aec2bc235a9922
Diego Brunetti Teixeira <diegoonsoftware@gmail.com>
2020-08-30 19:59:37 -0300

    Added solution for 'ISBN Verifier' problem

--- isbn-verifier/lib/isbn_verifier.ex
+++ isbn-verifier/lib/isbn_verifier.ex
@@ -1,5 +1,5 @@
 defmodule IsbnVerifier do
-  @allowed_chars ~r/[[:digit:][:upper:]]/u
+  @allowed_chars ~r/[^[:digit:][:upper:]]/u
   @doc """
     Checks if a string is a valid ISBN-10 identifier
 
@@ -20,9 +20,9 @@ defmodule IsbnVerifier do
   end
 
   defp format(isbn) do
-    @allowed_chars
-    |> Regex.scan(isbn)
-    |> List.flatten()
+    isbn
+    |> String.replace(@allowed_chars, "")
+    |> to_charlist()
     |> Enum.map(&to_integer/1)
   end
 
@@ -37,7 +37,7 @@ defmodule IsbnVerifier do
 
   defp apply_formula(_), do: false
 
-  defp to_integer(<<digit>>) when digit == ?X, do: 10
-  defp to_integer(<<digit>>) when digit >= ?0 and digit <= ?9, do: String.to_integer("#{digit}")
-  defp to_integer(_), do: 666
+  defp to_integer(?X), do: 10
+  defp to_integer(digit) when digit in ?0..?9, do: digit - 48
+  defp to_integer(_invalid), do: 666
 end
